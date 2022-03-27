98cd096b174bf112b600a66256e90cfe54712b3b
Roney Fraga Souza <roneyfraga@gmail.com>
2022-03-27 10:52:23 -0400

    save

---
+++ renv_nvim_setup.R
@@ -0,0 +1,7 @@
+
+renv::settings$ignored.packages("nvimcom")
+install.packages('devtools')
+devtools::install("~/.vim/plugged/Nvim-R/R/nvimcom")
+devtools::install_github('jalvesaq/colorout')
+install.packages("languageserver")
+install.packages("conflicted")
